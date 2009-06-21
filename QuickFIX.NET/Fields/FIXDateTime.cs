235d9c63c418963bae5eaeb9988e5adbf5c099d6
kennystone <kennethstone@gmail.com>
2009-06-21 11:00:48 -0500

    test for FIXDateTime obj

--- QuickFIX.NET/Fields/FIXDateTime.cs
+++ QuickFIX.NET/Fields/FIXDateTime.cs
@@ -7,17 +7,23 @@ namespace QuickFIX.NET.Fields
 {
     public sealed class FIXDateTime
     {
-        public FIXDateTime() { }
-
         public FIXDateTime(DateTime dt)
         {
             this._dt = new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second);
         }
 
+        #region Properties
+        public DateTime DateTimeObj 
+        {
+            get { return _dt; }
+            set { _dt = value; }
+        }
+        #endregion
+
         //YYYYMMDD-HH:MM:SS
         public override string ToString()
         {
-            return String.Format("{0:yyyyMMDD-HH:mm:ss}", _dt);
+            return String.Format("{0:yyyyMMdd-HH:mm:ss}", _dt);
         }
 
 
