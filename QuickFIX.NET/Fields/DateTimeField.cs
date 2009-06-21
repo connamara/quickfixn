7accf9d4bed643a36932e8d7f350803c8a42bd86
kennystone <kennethstone@gmail.com>
2009-06-21 10:07:12 -0500

    DateTime field

---
+++ QuickFIX.NET/Fields/DateTimeField.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuickFIX.NET.Fields
+{
+    public class DateTimeField : FieldBase<FIXDateTime>
+    {
+        public DateTimeField(int tag, FIXDateTime dt)
+            : base(tag, dt) { }
+
+        // quickfix compat
+        public FIXDateTime getValue()
+        { return Obj; }
+
+        public void setValue(FIXDateTime dt)
+        { Obj = dt; }
+    }
+}
---
+++ QuickFIX.NET/Fields/FIXDateTime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuickFIX.NET.Fields
+{
+    public sealed class FIXDateTime
+    {
+        public FIXDateTime() { }
+
+        public FIXDateTime(DateTime dt)
+        {
+            this._dt = new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, dt.Minute, dt.Second);
+        }
+
+        //YYYYMMDD-HH:MM:SS
+        public override string ToString()
+        {
+            return String.Format("{0:yyyyMMDD-HH:mm:ss}", _dt);
+        }
+
+
+        #region Private members
+        private DateTime _dt;
+        #endregion
+    }
+}
