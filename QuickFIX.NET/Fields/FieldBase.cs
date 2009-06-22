e51a0fdcbba4da862fe60fa96ddcc3168076c0ca
kennystone <kennethstone@gmail.com>
2009-06-21 20:52:53 -0500

    minor

--- QuickFIX.NET/Fields/FieldBase.cs
+++ QuickFIX.NET/Fields/FieldBase.cs
@@ -2,6 +2,10 @@
 
 namespace QuickFIX.NET.Fields
 {
+    /// <summary>
+    /// Base class for all field types
+    /// </summary>
+    /// <typeparam name="T">Internal storage type</typeparam>
     public abstract class FieldBase<T>
     {
         public FieldBase(int tag, T obj)
@@ -33,7 +37,9 @@ namespace QuickFIX.NET.Fields
         }
         #endregion
 
-        // tag=val
+        /// <summary>
+        /// returns full fix string: tag=val
+        /// </summary>
         public string toStringField()
         {
             if( _changed.Equals( true ))
@@ -41,18 +47,30 @@ namespace QuickFIX.NET.Fields
             return _stringField;
         }
 
+        /// <summary>
+        /// returns formatted string for fix
+        /// </summary>
         public override string ToString()
         {
             return makeString();
         }
 
+        /// <summary>
+        /// length of formatted field (including tag=val\001)
+        /// </summary>
         public int getLength()
         {
             return _stringField.Length + 1; // +1 for SOH
         }
 
+        /// <summary>
+        /// checksum
+        /// </summary>
         public int getTotal()
         {
+            if( _changed.Equals(true) )
+              makeStringField();
+
             int sum = 0;
             foreach (char c in _stringField)
             {
@@ -63,6 +81,9 @@ namespace QuickFIX.NET.Fields
 
         protected abstract string makeString();
 
+        /// <summary>
+        /// returns tag=val
+        /// </summary>
         private void makeStringField()
         {
             _stringField = Tag + "=" + ToString();
