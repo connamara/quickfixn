67f672c45769fa0b8e8c7f6852b8e61e56bb1c91
Kenny Stone <kennethstone@gmail.com>
2009-06-21 23:20:24 -0500

    fix generate

--- generator/generate.rb
+++ generator/generate.rb
@@ -1,23 +1,26 @@
-require 'rexml'
+require 'rexml/document'
 require 'field'
 require 'field_gen'
 
-class Generator
+class Generator  
   def self.generate
     generator = Generator.new
-    generator.generate_fields( fix44 )
+    generator.generate_fields
   end
   
   def initialize
-    @fix44 = generator.loadXML('../spec/fix/FIX44.xml')
+    @fix44 = loadXML('../spec/fix/FIX44.xml')
     @fields = Field.process_all( @fix44.elements["fix/fields"] )
   end
-  
+    
   def loadXML( filename )
    return( REXML::Document.new( File.new( filename ) ))
   end
   
   def generate_fields
-    puts @fields.inspect
+    FieldGen.generate_tags( @fields, '../QuickFIX.NET/Fields' )
   end
 end
+
+
+Generator.generate
