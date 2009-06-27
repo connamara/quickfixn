d1d3f7ede9fc014a92892653f59d65501f9c3881
shanetrotter <shane455@gmail.com>
2009-06-27 10:12:49 -0500

    updated tests to include testing client->server message sending

--- UnitTests/SocketInitiatorTests.cs
+++ UnitTests/SocketInitiatorTests.cs
@@ -16,7 +16,7 @@ namespace UnitTests
     public class SocketInitiatorTests
     {
         [Test]
-        public void TestConnect()
+        public void TestConnectAndReceive()
         {
             IPAddress localAddr = IPAddress.Parse("127.0.0.1");
             TcpListener listener = new TcpListener(localAddr, 56123);
@@ -32,13 +32,28 @@ namespace UnitTests
             i.Start();
 
             Thread.Sleep(100);
+            // Server sends initiator a message.
             _clientSocket.Send(Encoding.UTF8.GetBytes("TESTING123\n"));
             Thread.Sleep(100);
 
+            // Assert that the initiator is connected and receives it.
             Assert.That(_clientSocket.Connected, Is.True);
             Assert.That(i.Connected, Is.True);
             Assert.That(_lastReceived, Is.EqualTo("TESTING123"));
+            
+            // Send message from initiator to server.
+            string testSend = "TESTING456";
+            i.Send(testSend);
 
+            byte[] r = new byte[256];
+            _clientSocket.Receive(r);
+            string received = Encoding.UTF8.GetString(r);
+            Thread.Sleep(100);
+
+            // Assert that dummy server receives our test message.
+            // Note the substring, our dummy TcpListener is only receiving raw socket blocks of 256 bytes,
+            // so we only assert that what we sent is contained in the first part of the 256 byte block.
+            Assert.That(received.Substring(0, testSend.Length), Is.EqualTo(testSend));
 
             _clientSocket.Shutdown(SocketShutdown.Both);
             i.Close();
