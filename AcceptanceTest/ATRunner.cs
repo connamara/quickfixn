4d9f07bc0f9c0b9714993d0cc1fe931479341461
Kenny Stone <kennethstone@gmail.com>
2011-06-20 16:02:58 -0500

    at runner

---
+++ AcceptanceTest/ATRunner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcceptanceTest
+{
+    class ATRunner
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TODO: a real application for running the tests");
+        }
+    }
+}
