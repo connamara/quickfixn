51db055a7e277583064a03de9c879aec5c951959
Mike Gatny <mgatny@connamara.com>
2011-06-20 23:54:09 -0700

    Working top-down to get the TradeClient example ported. Stubbed in SessionSettings and friends.
    Former-commit-id: 8b785f9dc803f531734a8ed3d9f4dddabd369317 [formerly c6137c5aadcc2b0d110833997216ccd3064412bc] [formerly e725a5483e6a77f76fda4bf76d0f785fe8f8e9b0 [formerly 2e2fa8bfc3ebc6e8862160ff0b9d2af85c5a4e25]]
    Former-commit-id: 8261b9c7006073f266ae9244a9c60e1e16c0cd73 [formerly 637e538e752eed983379932924b9c28873ee3771]
    Former-commit-id: 2c398ae7ff23a49305ff171cfc01e7170abff38c

---
+++ QuickFIX.NET/FileStoreFactory.cs
@@ -0,0 +1,11 @@
+﻿
+namespace QuickFIX.NET
+{
+    public class FileStoreFactory : MessageStoreFactory
+    {
+        public FileStoreFactory(SessionSettings settings)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ QuickFIX.NET/ScreenLogFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuickFIX.NET
+{
+    public class ScreenLogFactory : LogFactory
+    {
+        public ScreenLogFactory(SessionSettings settings)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
