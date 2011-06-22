16e261dec661d64c1c09a0194c2e755b6fe751d6
Mike Gatny <mgatny@connamara.com>
2011-06-22 14:36:25 -0700

    Settings unit test complete
    Former-commit-id: 55b14db1a60c89940176e03c978bb0bb50c9b440 [formerly 11d8cba6eb398b301ef9991fb31ced50b5551f36] [formerly 2408c3c7fbf0572944caadfdb569b81515c32f00 [formerly af34c6e8a6e5635a3bdfe7d5033d9246b2ea33f9]]
    Former-commit-id: c2ef99bc05a106ad7614cd65bdf048bbc99564e6 [formerly f3b71b18d2beff46a39f0237f53b8814ff950100]
    Former-commit-id: b604559143acb0105261785cb4ee9ea797945490

--- QuickFIX.NET/Settings.cs
+++ QuickFIX.NET/Settings.cs
@@ -6,6 +6,60 @@ namespace QuickFix
     {
         private LinkedList<QuickFix.Dictionary> sections_ = new LinkedList<QuickFix.Dictionary>();
 
+        public Settings(System.IO.TextReader conf)
+        {
+            QuickFix.Dictionary currentSection = null;
+
+            string line = null;
+            while ((line = conf.ReadLine()) != null)
+            {
+                line = line.Trim();
+                if (IsComment(line))
+                {
+                    continue;
+                }
+                else if (IsSection(line))
+                {
+                    currentSection = Add(new Dictionary(SplitSection(line)));
+                }
+                else if (IsKeyValue(line))
+                {
+                    string[] kv = line.Split('=');
+                    if (currentSection != null)
+                        currentSection.SetString(kv[0].Trim(), kv[1].Trim());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Strip the outer '[' and ']' from the section name, e.g. '[DEFAULT]' becomes 'DEFAULT'
+        /// </summary>
+        /// <param name="s">the section name</param>
+        /// <returns></returns>
+        public static string SplitSection(string s)
+        {
+            return s.Trim('[', ']').Trim();
+        }
+
+        public static bool IsComment(string s)
+        {
+            if (s.Length < 1)
+                return false;
+            return '#' == s[0];
+        }
+
+        public static bool IsKeyValue(string s)
+        {
+            return s.IndexOf('=') != -1;
+        }
+
+        public static bool IsSection(string s)
+        {
+            if (s.Length < 2)
+                return false;
+            return s[0] == '[' && s[s.Length - 1] == ']';
+        }
+
         public QuickFix.Dictionary Add(QuickFix.Dictionary section)
         {
             sections_.AddLast(section);
--- UnitTests/SettingsTest.cs
+++ UnitTests/SettingsTest.cs
@@ -1,11 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using QuickFix;
 
 namespace UnitTests
 {
-    class SettingsTest
+    [TestFixture]
+    public class SettingsTest
     {
+        [Test]
+        public void Load()
+        {
+            string configuration = new System.Text.StringBuilder()
+                .AppendLine("[FOO]").AppendLine("bar=24").AppendLine("baz=moo")
+                .AppendLine("[OREN]").AppendLine("Nero=TW").AppendLine("#Nero=IGNOREME")
+                .AppendLine("# this is a comment")
+                .AppendLine("#[OREN]").AppendLine("#Nero=IGNOREME")
+                .AppendLine("[OREN]").AppendLine("ISLD=Nero").AppendLine("STUFF=./\\:")
+                .AppendLine("[NERO]").AppendLine("WINDIR=D:\\This Is\\A-Directory\\Connamara\\")
+                .AppendLine("WINFILE=D:\\Program Files\\Tomcat 4.1\\webapps\\mgatny\\WEB-INF\\connamara.cfg")
+                .AppendLine("UNIXDIR=/home/mgatny/This Is/A Directory/ok/")
+                .AppendLine("stripspace=last spaces stripped   ")
+                .AppendLine("#Nero=IGNOREME")
+                .ToString();
+            Settings settings = new Settings(new System.IO.StringReader(configuration));
+
+            LinkedList<QuickFix.Dictionary> foo = settings.Get("FOO");
+            Assert.That(foo.Count, Is.EqualTo(1));
+            Assert.That(foo.First.Value.GetLong("bar"), Is.EqualTo(24));
+            Assert.That(foo.First.Value.GetString("baz"), Is.EqualTo("moo"));
+            Assert.That(foo.First.Value.GetString("baz"), Is.EqualTo("moo"));
+            Assert.That(foo.First.Value.Count, Is.EqualTo(2));
+
+            LinkedList<QuickFix.Dictionary> oren = settings.Get("OREN");
+            Assert.That(oren.Count, Is.EqualTo(2));            
+            Assert.That(oren.First.Value.Count, Is.EqualTo(1));
+            Assert.That(oren.First.Value.GetString("Nero"), Is.EqualTo("TW"));
+            oren.RemoveFirst();
+            Assert.That(oren.First.Value.Count, Is.EqualTo(2));
+            Assert.That(oren.First.Value.GetString("ISLD"), Is.EqualTo("Nero"));
+            Assert.That(oren.First.Value.GetString("STUFF"), Is.EqualTo("./\\:"));
+
+            LinkedList<QuickFix.Dictionary> nero = settings.Get("NERO");
+            Assert.That(nero.Count, Is.EqualTo(1));
+            Assert.That(nero.First.Value.Count, Is.EqualTo(4));
+            Assert.That(nero.First.Value.GetString("WINDIR"), Is.EqualTo("D:\\This Is\\A-Directory\\Connamara\\"));
+            Assert.That(nero.First.Value.GetString("UNIXDIR"), Is.EqualTo("/home/mgatny/This Is/A Directory/ok/"));
+            Assert.That(nero.First.Value.GetString("WINFILE"), Is.EqualTo("D:\\Program Files\\Tomcat 4.1\\webapps\\mgatny\\WEB-INF\\connamara.cfg"));
+            Assert.That(nero.First.Value.GetString("stripspace"), Is.EqualTo("last spaces stripped"));
+        }
     }
 }
