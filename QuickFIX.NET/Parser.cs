04a11d6649e1cca1a4839aff3ffdf74009d1e395
Mike Gatny <mgatny@connamara.com>
2011-07-05 15:38:36 -0700

    Initiator accepts logon response from acceptor
    Former-commit-id: d9dbdd02e29bbfeb5b03ac6fc8442f1d7d558dea [formerly 1f96d26da2d727a013638c1a358f415c5efee406] [formerly 1f3be40958647e4553e4f9ec080d83e037752873 [formerly 773eab2ec1c23f40ef0025c0cfcf7ac7aca602c7]]
    Former-commit-id: b156535c4fa54eb6a1c05fc5eef7d4e269ae6484 [formerly 68986835ba76df6ea13362675d3c4487f715d8a5]
    Former-commit-id: 2a1b32cd2f5477684a109aed9246409504de0f91

--- QuickFIX.NET/Parser.cs
+++ QuickFIX.NET/Parser.cs
@@ -7,9 +7,24 @@ namespace QuickFix
     {
         private StringBuilder buffer_ = new StringBuilder();
 
+        /// <summary>
+        /// FIXME
+        /// </summary>
+        /// <param name="data"></param>
         public void AddToStream(string data)
         {
             buffer_.Append(data);
+        }
+
+        /// <summary>
+        /// FIXME
+        /// </summary>
+        /// <returns></returns>
+        public string ReadFixMessage()
+        {
+            string msg = buffer_.ToString();
+            buffer_ = new StringBuilder();
+            return msg;
         }
     }
 }
