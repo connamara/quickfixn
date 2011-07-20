e747a1e798354b97a9c3a6448f0e24886ccfef64
Mike Gatny <mgatny@connamara.com>
2011-07-19 22:03:36 -0700

    use 'call' when calling runat

--- acceptance_test.bat
+++ acceptance_test.bat
@@ -4,7 +4,7 @@ set RESULT=0
 
 pushd AcceptanceTest
     del TestResult.xml AcceptanceTests.html
-    runat.bat release 5001 > TestResult.xml
+    call runat.bat release 5001 > TestResult.xml
     if ERRORLEVEL 1 set RESULT=1
     xsltproc.exe -o AcceptanceTests.html at.xsl TestResult.xml
 popd
