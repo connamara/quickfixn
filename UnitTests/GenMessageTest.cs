948fbf6ab46b2e3f1f65f3b5ef3cb9f04d00eeb9
Kenny Stone <kennethstone@gmail.com>
2011-07-25 16:05:26 -0500

    get/set, ut
    Former-commit-id: c6ddd74da1697e33d446e4c5098cd3263efc7dc3 [formerly 3e8787073a54b8198721a5c507c05fc7a709fc08] [formerly f8264c66ea507a4fa78dd233603e5cc691fbef0e [formerly 86033944be26ebf548ffe71f9238c71303904cc4]]
    Former-commit-id: 72826690ebd7924beb5d95019863a3c6067c944e [formerly 6ab600f68edb10d91680546d3412726e5bda3c72]
    Former-commit-id: 412836b1b93fc10b6814de361a36fedc845bb9e2

---
+++ UnitTests/GenMessageTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using QuickFix;
+using QuickFix.Fields;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class GenMessageTests
+    {
+        [Test]
+        public void TCRFieldProperties()
+        {
+            Decimal val = new Decimal(3.232535);
+            QuickFix.FIX44.TradeCaptureReport tcr = new QuickFix.FIX44.TradeCaptureReport();
+            tcr.AvgPx = new AvgPx(val);
+            Assert.That(tcr.AvgPx.getValue(), Is.EqualTo(val));
+        }
+
+        [Test]
+        public void TCRFieldGetter()
+        {
+            AvgPx avgPx = new AvgPx(new Decimal(10.5));
+            QuickFix.FIX44.TradeCaptureReport tcr = new QuickFix.FIX44.TradeCaptureReport();
+            tcr.setField(avgPx);
+            Assert.That(tcr.AvgPx.getValue(), Is.EqualTo(avgPx.getValue()));
+            AvgPx avgPx2 = new AvgPx();
+            tcr.getField(avgPx2);
+            Assert.That(avgPx2.getValue(), Is.EqualTo(avgPx.getValue()));
+        }
+
+        [Test]
+        public void TCRFieldSetterTypeSafeOldWay()
+        {
+            AvgPx avgPx = new AvgPx(new Decimal(10.5));
+            QuickFix.FIX44.TradeCaptureReport tcr = new QuickFix.FIX44.TradeCaptureReport();
+            tcr.set(avgPx);
+            Assert.That(tcr.AvgPx.getValue(), Is.EqualTo(avgPx.getValue()));
+        }
+
+
+        [Test]
+        public void TCRFieldGetterTypeSafeOldWay()
+        {
+            AvgPx avgPx = new AvgPx(new Decimal(10.5));
+            QuickFix.FIX44.TradeCaptureReport tcr = new QuickFix.FIX44.TradeCaptureReport();
+            tcr.set(new AvgPx(new Decimal(10.5)));
+            tcr.get(avgPx);
+            Assert.That(tcr.AvgPx.getValue(), Is.EqualTo(avgPx.getValue()));
+        }
+    }
+}
