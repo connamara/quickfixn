537f99bef0e400d4f323e0998fcd328874ccd3c6
Mike Gatny <mgatny@connamara.com>
2011-08-01 15:52:48 -0500

    fix acceptance_test.bat errors
    Former-commit-id: 33421e5036eb830002690d396c8f09a06c6c9d3a [formerly 69999b8b817f7e08eb6c3982a1578bfe77a0b529] [formerly b944b0bdbbc2064afaefeb2c9fa2334065f3328f [formerly 9a0ad80359c36c6e0c2c89b3d243f2f28c040a4f]]
    Former-commit-id: fda7b47c7ca6698302437f0f433d2f0aa3063c46 [formerly 329ed47324eabb7edd2f051fa3010318c6843c31]
    Former-commit-id: 86a32c2a0f61bdd1704cf2e7d86a96d799be3461

--- AcceptanceTest/runat.bat
+++ AcceptanceTest/runat.bat
@@ -2,14 +2,14 @@
 if "%1" == "" goto usage
 if "%2" == "" goto usage
 
-set TESTS=definitions\server\fix42\*.def
-if "%3" == "" goto getconfig
+set TESTS="definitions/server/fix42/*.def"
+if "%3" == "" goto getconfig:
 set TESTS=%3
 goto getconfig
 
 :getconfig
-if "%1" == "debug" goto debug:
-if "%1" == "release" goto release:
+if "%1" == "debug" goto debug
+if "%1" == "release" goto release
 goto usage
 
 :debug
@@ -23,6 +23,7 @@ goto start
 :start
 
 call setup.bat %2
+
 REM release\at\atrun -t run -s "%DIR%\AcceptanceTest.exe cfg\at.cfg" -d . -c "ruby Runner.rb 127.0.0.1 %2 definitions\server\fix40\*.def definitions\server\fix41\*.def definitions\server\fix42\*.def definitions\server\fix43\*.def definitions\server\fix44\*.def definitions\server\fix50\*.def definitions\server\fix50sp1\*.def definitions\server\fix50sp2\*.def" -i .\
 release\at\atrun -t run -s "%DIR%\AcceptanceTest.exe cfg\at.cfg" -d . -c "ruby Runner.rb 127.0.0.1 %2 %TESTS%" -i .\ 
 goto quit
