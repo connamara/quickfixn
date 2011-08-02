1b97e24b7003209b06e2c15c35fe0286dccff6f6
Mike Gatny <mgatny@connamara.com>
2011-08-02 09:47:10 -0500

    AT 10 (seq reset) is passing

--- QuickFIX.NET/FixValues.cs
+++ QuickFIX.NET/FixValues.cs
@@ -4,8 +4,23 @@ namespace QuickFix
     /// <summary>
     /// TODO generate this class
     /// </summary>
-    public class FixValues
+    namespace FixValues
     {
-        public const int SessionRejectReason_VALUE_IS_INCORRECT = 5;
+        public class MsgType
+        {
+            public const string HEARTBEAT = "0";
+            public const string TEST_REQUEST = "1";
+            public const string RESEND_REQUEST = "2";
+            public const string REJECT = "3";
+            public const string SEQUENCE_RESET = "4";
+            public const string LOGOUT = "5";
+            public const string LOGON = "A";
+            public const string NEW_ORDER_SINGLE = "D";
+        }
+
+        public class SessionRejectReason
+        {
+            public const int VALUE_IS_INCORRECT = 5;
+        }
     }
 }
