e467bfe137ac8f81e044a903e858eee6a7c072cf
Kenny Stone <kennethstone@gmail.com>
2011-08-19 14:48:07 -0500

    we are rewriting data dictionary, this is where the rewrite is stashed for now
    Former-commit-id: f0a1e8a137be820b48e9c0ed19744d6749087f9a [formerly 0dae3cfbb424a115dc93813f164b7cdc433aae52] [formerly 7a3c65f90291f006cb97977802dc771db10d4195 [formerly 320313579f0290b832ea3d2d36bcf71241dd5014]]
    Former-commit-id: b858e6f16d7212cc31dd90cfeff1d6c324483a46 [formerly 3b8e57c6f29998ae5539dc2933e8d04a5b3458cd]
    Former-commit-id: 8f98d6853a8d1b75c17680bf7848f3aca99b53a7

---
+++ UnitTests/DD2Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using QuickFix;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class DD2Tests
+    {
+        [Test]
+        public void VersionTest()
+        {
+            QuickFix.DataDictionary.DD2 dd = new QuickFix.DataDictionary.DD2();
+            dd.Load("../../../spec/fix/FIX44.xml");
+            Assert.That(dd.MajorVersion, Is.EqualTo("4"));
+            Assert.That(dd.MinorVersion, Is.EqualTo("4"));
+            Assert.That(dd.Version, Is.EqualTo("FIX.4.4"));
+        }
+
+        [Test]
+        public void LoadFieldsTest()
+        {
+            QuickFix.DataDictionary.DD2 dd = new QuickFix.DataDictionary.DD2();
+            dd.Load("../../../spec/fix/FIX44.xml");
+            Assert.That(dd.FieldsByTag[1].Name, Is.EqualTo("Account"));
+            Assert.That(dd.FieldsByName["Account"].Tag, Is.EqualTo(1));
+            Assert.That(dd.FieldsByTag[1].Enums.Count, Is.EqualTo(0));
+            Assert.That(dd.FieldsByTag[QuickFix.Fields.Tags.StatusValue].Enums.Count, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void FieldHasValueTest()
+        {
+            QuickFix.DataDictionary.DD2 dd = new QuickFix.DataDictionary.DD2();
+            dd.Load("../../../spec/fix/FIX44.xml");
+            Assert.That(dd.FieldHasValue(QuickFix.Fields.Tags.StatusValue, "1"), Is.EqualTo(true));
+            Assert.That(dd.FieldHasValue(QuickFix.Fields.Tags.StatusValue, "CONNECTED"), Is.EqualTo(false));
+        }
+    }
+}
