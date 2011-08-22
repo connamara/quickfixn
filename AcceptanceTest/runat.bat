ada3766808c60eacd149fd0b24c214fcb871e466
Mike Gatny <mgatny@connamara.com>
2011-08-22 07:46:46 -0500

    Proper exit code to calling shell

--- AcceptanceTest/runat.bat
+++ AcceptanceTest/runat.bat
@@ -1,4 +1,7 @@
 @echo off
+
+set RESULT=0
+
 if "%1" == "" goto usage
 if "%2" == "" goto usage
 
@@ -26,10 +29,14 @@ call setup.bat %2
 
 REM release\at\atrun -t run -s "%DIR%\AcceptanceTest.exe cfg\at.cfg" -d . -c "ruby Runner.rb 127.0.0.1 %2 definitions\server\fix40\*.def definitions\server\fix41\*.def definitions\server\fix42\*.def definitions\server\fix43\*.def definitions\server\fix44\*.def definitions\server\fix50\*.def definitions\server\fix50sp1\*.def definitions\server\fix50sp2\*.def" -i .\  -o TestResult.xml 
 release\at\atrun -t run -s "%DIR%\AcceptanceTest.exe cfg\at.cfg" -d . -c "ruby Runner.rb 127.0.0.1 %2 %TESTS%" -i .\  -o TestResult.xml 
+if ERRORLEVEL 1 set RESULT=1
 type TestResult.xml
 goto quit
 
 :usage
 echo "Usage: runat [release | debug] [TESTFILE...]"
+set RESULT=1
 
 :quit
+exit /B %RESULT%
+
