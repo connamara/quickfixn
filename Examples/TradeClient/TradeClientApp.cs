d6777c03772b4fe7204ea589f80a3020fc49eb5a
Grant Birchmeier <gbirchmeier@connamara.com>
2011-08-29 20:30:10 -0500

    Coding done for 4.2 messages.  Not confirmed working yet.

--- Examples/TradeClient/TradeClientApp.cs
+++ Examples/TradeClient/TradeClientApp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using QuickFix;
+using QuickFix.Fields;
 
 namespace TradeClient
 {
@@ -244,26 +245,231 @@ namespace TradeClient
         }
 
 
-
-
         private QuickFix.FIX42.NewOrderSingle QueryNewOrderSingle42()
         {
-            throw new System.NotImplementedException("not done");
+            QuickFix.Fields.OrdType ordType = null;
+
+            QuickFix.FIX42.NewOrderSingle newOrderSingle = new QuickFix.FIX42.NewOrderSingle(
+                QueryClOrdID(),
+                new HandlInst('1'),
+                QuerySymbol(),
+                QuerySide(),
+                new TransactTime(DateTime.Now),
+                ordType = QueryOrdType());
+
+            newOrderSingle.set(QueryOrderQty());
+            newOrderSingle.set(QueryTimeInForce());
+            if (ordType.getValue() == OrdType.LIMIT || ordType.getValue() == OrdType.STOP_LIMIT)
+                newOrderSingle.set(QueryPrice());
+            if (ordType.getValue() == OrdType.STOP || ordType.getValue() == OrdType.STOP_LIMIT)
+                newOrderSingle.set(QueryStopPx());
+
+            QueryHeader(newOrderSingle.Header);
+            return newOrderSingle;
         }
 
         private QuickFix.FIX42.OrderCancelRequest QueryOrderCancelRequest42()
         {
-            throw new System.NotImplementedException("not done");
+            QuickFix.FIX42.OrderCancelRequest orderCancelRequest = new QuickFix.FIX42.OrderCancelRequest(
+                QueryOrigClOrdID(),
+                QueryClOrdID(),
+                QuerySymbol(),
+                QuerySide(),
+                new TransactTime(DateTime.Now));
+
+            orderCancelRequest.set(QueryOrderQty());
+            QueryHeader(orderCancelRequest.Header);
+            return orderCancelRequest;
         }
 
         private QuickFix.FIX42.OrderCancelReplaceRequest QueryCancelReplaceRequest42()
         {
-            throw new System.NotImplementedException("not done");
+            QuickFix.FIX42.OrderCancelReplaceRequest ocrr = new QuickFix.FIX42.OrderCancelReplaceRequest(
+                QueryOrigClOrdID(),
+                QueryClOrdID(),
+                new HandlInst('1'),
+                QuerySymbol(),
+                QuerySide(),
+                new TransactTime(DateTime.Now),
+                QueryOrdType());
+
+            if(QueryConfirm("New price"))
+                ocrr.set( QueryPrice());
+            if(QueryConfirm("New quantity"))
+                ocrr.set(QueryOrderQty());
+
+            QueryHeader(ocrr.Header);
+            return ocrr;
         }
 
         private QuickFix.FIX42.MarketDataRequest QueryMarketDataRequest42()
         {
-            throw new System.NotImplementedException("not done");
+            MDReqID mdReqID = new MDReqID( "MARKETDATAID" );
+            SubscriptionRequestType subType = new SubscriptionRequestType(SubscriptionRequestType.SNAPSHOT);
+            MarketDepth marketDepth = new MarketDepth(0);
+
+            QuickFix.FIX42.MarketDataRequest.NoMDEntryTypes marketDataEntryGroup = new QuickFix.FIX42.MarketDataRequest.NoMDEntryTypes();
+            marketDataEntryGroup.set( new MDEntryType(MDEntryType.BID) );
+
+            QuickFix.FIX42.MarketDataRequest.NoRelatedSym symbolGroup = new QuickFix.FIX42.MarketDataRequest.NoRelatedSym();
+            symbolGroup.set(new Symbol("LNUX"));
+
+            QuickFix.FIX42.MarketDataRequest message = new QuickFix.FIX42.MarketDataRequest( mdReqID, subType, marketDepth );
+            message.AddGroup( marketDataEntryGroup );
+            message.AddGroup( symbolGroup );
+
+            QueryHeader( message.Header );
+
+            Console.WriteLine(message.ToString());
+            return message;
         }
+
+
+        #region field query private methods
+        private ClOrdID QueryClOrdID()
+        {
+            Console.WriteLine();
+            Console.Write("ClOrdID? ");
+            return new ClOrdID(Console.ReadLine().Trim());
+        }
+
+        private OrigClOrdID QueryOrigClOrdID()
+        {
+            Console.WriteLine();
+            Console.Write("OrigClOrdID? ");
+            return new OrigClOrdID(Console.ReadLine().Trim());
+        }
+
+        private Symbol QuerySymbol()
+        {
+            Console.WriteLine();
+            Console.Write("Symbol? ");
+            return new Symbol(Console.ReadLine().Trim());
+        }
+
+        private Side QuerySide()
+        {
+            Console.WriteLine();
+            Console.WriteLine("1) Buy");
+            Console.WriteLine("2) Sell");
+            Console.WriteLine("3) Sell Short");
+            Console.WriteLine("4) Sell Short Exempt");
+            Console.WriteLine("5) Cross");
+            Console.WriteLine("6) Cross Short");
+            Console.WriteLine("7) Cross Short Exempt");
+            Console.Write("Side? ");
+            string s = Console.ReadLine().Trim();
+
+            char c = ' ';
+            switch (s)
+            {
+                case "1": c = Side.BUY; break;
+                case "2": c = Side.SELL; break;
+                case "3": c = Side.SELL_SHORT; break;
+                case "4": c = Side.SELL_SHORT_EXEMPT; break;
+                case "5": c = Side.CROSS; break;
+                case "6": c = Side.CROSS_SHORT; break;
+                case "7": c = 'A'; break;
+                default: throw new Exception("unsupported input");
+            }
+            return new Side(c);
+        }
+
+        private OrdType QueryOrdType()
+        {
+            Console.WriteLine();
+            Console.WriteLine("1) Market");
+            Console.WriteLine("2) Limit");
+            Console.WriteLine("3) Stop");
+            Console.WriteLine("4) Stop Limit");
+            Console.Write("OrdType? ");
+            string s = Console.ReadLine().Trim();
+
+            char c = ' ';
+            switch (s)
+            {
+                case "1": c = OrdType.MARKET; break;
+                case "2": c = OrdType.LIMIT; break;
+                case "3": c = OrdType.STOP; break;
+                case "4": c = OrdType.STOP_LIMIT; break;
+                default: throw new Exception("unsupported input");
+            }
+            return new OrdType(c);
+        }
+
+        private OrderQty QueryOrderQty()
+        {
+            Console.WriteLine();
+            Console.Write("OrderQty? ");
+            return new OrderQty(Convert.ToDecimal(Console.ReadLine().Trim()));
+        }
+
+        private TimeInForce QueryTimeInForce()
+        {
+            Console.WriteLine();
+            Console.WriteLine("1) Day");
+            Console.WriteLine("2) IOC");
+            Console.WriteLine("3) OPG");
+            Console.WriteLine("4) GTC");
+            Console.WriteLine("5) GTX");
+            Console.Write("TimeInForce? ");
+            string s = Console.ReadLine().Trim();
+
+            char c = ' ';
+            switch (s)
+            {
+                case "1": c = TimeInForce.DAY; break;
+                case "2": c = TimeInForce.IMMEDIATE_OR_CANCEL; break;
+                case "3": c = TimeInForce.AT_THE_OPENING; break;
+                case "4": c = TimeInForce.GOOD_TILL_CANCEL; break;
+                case "5": c = TimeInForce.GOOD_TILL_CROSSING; break;
+                default: throw new Exception("unsupported input");
+            }
+            return new TimeInForce(c);
+        }
+
+        private Price QueryPrice()
+        {
+            Console.WriteLine();
+            Console.Write("Price? ");
+            return new Price(Convert.ToDecimal(Console.ReadLine().Trim()));
+        }
+
+        private StopPx QueryStopPx()
+        {
+            Console.WriteLine();
+            Console.Write("StopPx? ");
+            return new StopPx(Convert.ToDecimal(Console.ReadLine().Trim()));
+        }
+
+        private void QueryHeader(Header h)
+        {
+            h.setField(QuerySenderCompID());
+            h.setField(QueryTargetCompID());
+            if (QueryConfirm("Use a TargetSubID"))
+                h.setField(QueryTargetSubID());
+        }
+
+        private SenderCompID QuerySenderCompID()
+        {
+            Console.WriteLine();
+            Console.WriteLine("SenderCompID? ");
+            return new SenderCompID(Console.ReadLine().Trim());
+        }
+
+        private TargetCompID QueryTargetCompID()
+        {
+            Console.WriteLine();
+            Console.WriteLine("TargetCompID? ");
+            return new TargetCompID(Console.ReadLine().Trim());
+        }
+
+        private TargetSubID QueryTargetSubID()
+        {
+            Console.WriteLine();
+            Console.WriteLine("TargetSubID? ");
+            return new TargetSubID(Console.ReadLine().Trim());
+        }
+        #endregion
     }
 }
