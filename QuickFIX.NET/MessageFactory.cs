ef9648a8f9763792b5111bf93633e157d86bd6c9
Grant Birchmeier <gbirchmeier@connamara.com>
2011-08-30 14:21:53 -0500

    factory interface
    Former-commit-id: ffe057bef93d1b7271b8a8fb68b83b8a1ba96421 [formerly 23ef26f9d539853a9bcb45a7a289716cb58bb36f] [formerly 3e914be2d3f1cefd900ba979def69c494b03c11a [formerly 5d68cc92f16aa66499e78f93afe796928c12dd9d]]
    Former-commit-id: 0502c4c941779e645ee5babb5e4beafb79d9b86a [formerly c63b40d727c22e999ae0737f817fda0af6f32c83]
    Former-commit-id: 43695868c52ea560c3b8d9d1e8fbbff4e3ca125c

---
+++ QuickFIX.NET/MessageFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuickFix
+{
+    /// <summary>
+    /// Used by Session to create a Message of the appropriate type.
+    /// </summary>
+    public interface IMessageFactory
+    {
+        /// <summary>
+        /// Creates a message for a specified type and FIX version
+        /// </summary>
+        /// <param name="beginString">the FIX version (e.g. "FIX.4.2")</param>
+        /// <param name="msgType">the FIX message type (e.g. "D" for a NewOrderSingle)</param>
+        /// <returns>a message instance of proper derived type</returns>
+        Message create(string beginString, string msgType);
+
+        /// <summary>
+        /// Creates a group for the specified parent message type and
+        /// for the fields with the corresponding field ID
+        /// </summary>
+        /// <param name="beginString">the FIX version (e.g. "FIX.4.2")</param>
+        /// <param name="msgType">message type of the enclosing message (e.g. "D" for a NewOrderSingle</param>
+        /// <param name="correspondingFieldID">the fieldID of the field in the group</param>
+        /// <returns>group, or null if the group can't be created</returns>
+        Group create(string beginString, string msgType, int correspondingFieldID);
+    }
+}
