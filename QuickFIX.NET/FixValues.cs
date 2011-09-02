16fdc52b4b824a1f602b5e0b5d17f606c77f72e8
Kenny Stone <kennethstone@gmail.com>
2011-09-02 11:20:36 -0500

    generate business reject for unhandled message type

--- QuickFIX.NET/FixValues.cs
+++ QuickFIX.NET/FixValues.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace QuickFix
 {
     public class FixValue<T>
@@ -111,6 +112,22 @@ namespace QuickFix
             public SessionRejectReason(int value, string description)
                 : base(value, description)
             { }
+        }
+
+        public class BusinessRejectReason
+        {
+            public static readonly Dictionary<int, string> RejText = new Dictionary<int, string>()
+            {
+		        { Fields.BusinessRejectReason.OTHER, "Other" },
+        		{ Fields.BusinessRejectReason.UNKNOWN_ID, "Unknown ID" },
+        		{ Fields.BusinessRejectReason.UNKNOWN_SECURITY, "Unknown Security" },
+        		{ Fields.BusinessRejectReason.UNKNOWN_MESSAGE_TYPE, "Unsupported Message Type" },
+        		{ Fields.BusinessRejectReason.APPLICATION_NOT_AVAILABLE, "Application Not Available" },
+        		{ Fields.BusinessRejectReason.CONDITIONALLY_REQUIRED_FIELD_MISSING, "Conditionally Required Field Missing" },
+        		{ Fields.BusinessRejectReason.NOT_AUTHORIZED, "Not authorized" },
+        		{ Fields.BusinessRejectReason.DELIVERTO_FIRM_NOT_AVAILABLE_AT_THIS_TIME, "DeliverTo Firm Not Available At This Time" }
+            };
+
         }
     }
 }
