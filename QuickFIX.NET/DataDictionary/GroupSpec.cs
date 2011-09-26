15cf067a27d11408f77ba33d7915ba8ef31132cb
Kenny Stone <kennethstone@gmail.com>
2011-09-26 13:33:48 -0500

    missed add file

---
+++ QuickFIX.NET/DataDictionary/GroupSpec.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace QuickFix.DataDictionary
+{
+    public interface IGroupSpec
+    {
+        Boolean IsGroup(int tag);
+        Boolean IsField(int tag);
+        int Delim { get; set; }
+    }
+}
