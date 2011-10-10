cc00bdedb3f41c9dee48b6780716c05134c51a1d
U-IP-0AF85FB3\Administrator <Administrator@ip-0A487939.(none)>
2011-10-10 12:02:53 -0700

    renamed sleep script
    Former-commit-id: 87150876beb20e9cca9767bcbf8dff84668ce6de [formerly c21c068278b72058def64435026415c10746e86e] [formerly 2fdea9bd058bc0743a5d8b60572e81bcef61b806 [formerly e436a06635a5c4cd108c852040112aef935ccff3]]
    Former-commit-id: c390a788cfc70dadc7e32b1180c985bcd1e9c793 [formerly 60f9ed68b02ef00714544a81fe8fd1b77897277f]
    Former-commit-id: 358c513670cd949077c3a4d2a9ad964b954cc6b9

--- acceptance_test.bat
+++ acceptance_test.bat
@@ -5,43 +5,43 @@ pushd AcceptanceTest
     echo "40 tests result: %RESULT40%"
     xsltproc.exe -o AcceptanceTests_40.html at.xsl TestResult.xml
     
-    call sleep
+    call pause
     call runat release 5002 definitions/server/fix41/*.def cfg/at_41.cfg
     if ERRORLEVEL 1 set RESULT41=1
     echo "41 tests result: %RESULT41%"
     xsltproc.exe -o AcceptanceTests_41.html at.xsl TestResult.xml
     
-    call sleep
+    call pause
     call runat release 5003 definitions/server/fix42/*.def cfg/at_42.cfg
     if ERRORLEVEL 1 set RESULT42=1
     echo "42 tests result: %RESULT42%"
     xsltproc.exe -o AcceptanceTests_42.html at.xsl TestResult.xml
 
-    call sleep    
+    call pause    
     call runat release 5004 definitions/server/fix43/*.def cfg/at_43.cfg
     if ERRORLEVEL 1 set RESULT43=1
     echo "43 tests result: %RESULT43%"
     xsltproc.exe -o AcceptanceTests_43.html at.xsl TestResult.xml
     
-    call sleep
+    call pause
     call runat release 5005 definitions/server/fix44/*.def cfg/at_44.cfg
     if ERRORLEVEL 1 set RESULT44=1
     echo "44 tests result: %RESULT44%"
     xsltproc.exe -o AcceptanceTests_44.html at.xsl TestResult.xml
     
-    call sleep
+    call pause
     call runat release 5006 definitions/server/fix50/*.def cfg/at_50.cfg
     if ERRORLEVEL 1 set RESULT50=1
     echo "50 tests result: %RESULT50%"
     xsltproc.exe -o AcceptanceTests_50.html at.xsl TestResult.xml
     
-    call sleep
+    call pause
     call runat release 5007 definitions/server/fix50sp1/*.def cfg/at_50_sp1.cfg
     if ERRORLEVEL 1 set RESULT50SP1=1
     echo "50 SP1 tests result: %RESULT50SP1%"
     xsltproc.exe -o AcceptanceTests_50_SP1.html at.xsl TestResult.xml
     
-    call sleep
+    call pause
     call runat release 5008 definitions/server/fix50sp2/*.def cfg/at_50_sp2.cfg
     if ERRORLEVEL 1 set RESULT50SP2=1
     echo "50 SP2 tests result: %RESULT50SP2%"
