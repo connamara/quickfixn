a697c2cece1eedf30dcf53e273aec163b679efbf
chris busbey <cbusbey@connamara.com>
2011-10-12 16:02:34 -0700

    messages and event log in file log

--- QuickFIX.NET/FileLog.cs
+++ QuickFIX.NET/FileLog.cs
@@ -4,34 +4,46 @@ namespace QuickFix
     /// <summary>
     /// FIXME - right now this just works like ScreenLog, but writing to a file
     /// </summary>
-    public class FileLog : Log
+    public class FileLog : Log, System.IDisposable
     {
         private object sync_ = new object();
-        private SessionID sessionID_;
-        System.IO.StreamWriter file_;
+
+        private System.IO.StreamWriter messageLog_;
+        private System.IO.StreamWriter eventLog_;
+
+        public FileLog(string fileLogPath)
+        {
+            Init(fileLogPath, "GLOBAL");
+        }
 
         public FileLog(string fileLogPath, SessionID sessionID)
         {
-            sessionID_ = sessionID;
+            Init(fileLogPath, Prefix(sessionID));
+        }   
 
+        
+
+        private void Init(string fileLogPath, string prefix)
+        {
             if (!System.IO.Directory.Exists(fileLogPath))
                 System.IO.Directory.CreateDirectory(fileLogPath);
 
-            string fname = new System.Text.StringBuilder(sessionID_.BeginString)
-                .Append('-').Append(sessionID_.SenderCompID)
-                .Append('-').Append(sessionID_.TargetCompID)
-                .Append(".fixme.log")
-                .ToString();
-            fname = System.IO.Path.Combine(fileLogPath, fname);
-            file_ = new System.IO.StreamWriter(fname);
+            messageLog_ = new System.IO.StreamWriter(
+                System.IO.Path.Combine(fileLogPath, prefix + ".messages.current.log"));
+            eventLog_ = new System.IO.StreamWriter(
+                System.IO.Path.Combine(fileLogPath, prefix + ".event.current.log"));
         }
 
-        public void Destroy()
+        public static string Prefix(SessionID sessionID)
         {
-            lock (sync_)
-            {
-                file_.Close();
-            }
+            System.Text.StringBuilder prefix = new System.Text.StringBuilder(sessionID.BeginString)
+                .Append('-').Append(sessionID.SenderCompID)
+                .Append('-').Append(sessionID.TargetCompID);
+
+            if(sessionID.SessionQualifier.Length!=0)
+                prefix.Append('-').Append(sessionID.SessionQualifier);
+
+            return prefix.ToString();
         }
 
         #region Log Members
@@ -43,8 +55,8 @@ namespace QuickFix
         {
             lock (sync_)
             {
-                file_.WriteLine("<incoming> " + msg);
-                file_.Flush();
+                messageLog_.WriteLine("<incoming> " + msg);
+                messageLog_.Flush();
             }
         }
 
@@ -52,8 +64,8 @@ namespace QuickFix
         {
             lock (sync_)
             {
-                file_.WriteLine("<outgoing> " + msg);
-                file_.Flush();
+                messageLog_.WriteLine("<outgoing> " + msg);
+                messageLog_.Flush();
             }
         }
 
@@ -61,9 +73,19 @@ namespace QuickFix
         {
             lock (sync_)
             {
-                file_.WriteLine("<event> " + s);
-                file_.Flush();
+                eventLog_.WriteLine("<event> " + s);
+                eventLog_.Flush();
             }
+        }
+
+        #endregion
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            messageLog_.Close();
+            eventLog_.Close();
         }
 
         #endregion
---
+++ UnitTests/FileLogTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class FileLogTests
+    {
+        QuickFix.FileLog log;
+        QuickFix.SessionSettings settings;
+        QuickFix.SessionID sessionID;
+
+        [SetUp]
+        public void setup()
+        {
+            sessionID = new QuickFix.SessionID("FIX.4.2", "SENDERCOMP","TARGETCOMP");
+
+
+            QuickFix.Dictionary config = new QuickFix.Dictionary();
+            config.SetString(QuickFix.SessionSettings.CONNECTION_TYPE, "initiator");
+            config.SetString(QuickFix.SessionSettings.FILE_LOG_PATH, "log");
+
+            settings = new QuickFix.SessionSettings();
+            settings.Set(sessionID,config);
+   
+            QuickFix.FileLogFactory factory = new QuickFix.FileLogFactory(settings);
+            log = (QuickFix.FileLog)factory.Create(sessionID);
+        }
+
+        [TearDown]
+        public void teardown()
+        {
+            log.Dispose();
+        }
+
+        [Test]
+        public void testPrefix()
+        {
+            QuickFix.SessionID someSessionID = new QuickFix.SessionID("FIX.4.4", "sender", "target");
+            QuickFix.SessionID someSessionIDWithQualifier = new QuickFix.SessionID("FIX.4.3", "sender", "target","foo");
+
+            Assert.AreEqual("FIX.4.4-sender-target", QuickFix.FileLog.Prefix(someSessionID));
+            Assert.AreEqual("FIX.4.3-sender-target-foo", QuickFix.FileLog.Prefix(someSessionIDWithQualifier));
+        }
+
+        [Test]
+        public void testGenerateFileName()
+        {
+            log.OnEvent("some event");
+            log.OnIncoming("some incoming");
+            log.OnOutgoing("some outgoing");
+
+            Assert.That(System.IO.File.Exists("log/FIX.4.2-SENDERCOMP-TARGETCOMP.event.current.log"));
+            Assert.That(System.IO.File.Exists("log/FIX.4.2-SENDERCOMP-TARGETCOMP.messages.current.log"));
+        }
+    }
+}
