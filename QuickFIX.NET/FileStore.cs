90dba9cbc6954ab31c27b54eb2de5a1e9acb5ddc
chris busbey <cbusbey@connamara.com>
2011-10-13 11:53:24 -0500

    FileStore dummy, added FileStorePath configuration
    Former-commit-id: d5be7241d99e0b91f1700bac86c1aba536c3e5ac [formerly cfbd86ad7595b379299678cb4c5cdb937a421732] [formerly aeabff8868a8323f17a65877f6c9daff3898b4d7 [formerly 970d906b2ca25aeea096f222887e19ce50d607dd]]
    Former-commit-id: e816f3b57cd900be8bc7296d57df145d0b464656 [formerly c245ec156789814b001fd92b7ce286fe35d473a5]
    Former-commit-id: 6430426dc57a93e2d156029ae4fe446cb26af021

---
+++ QuickFIX.NET/FileStore.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuickFix
+{
+    class FileStore : MessageStore
+    {
+        public FileStore(string path, SessionID sessionID)
+        {
+
+        }
+
+        #region MessageStore Members
+
+        public bool Get(int startSeqNum, int endSeqNum, List<string> messages)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Set(int msgSeqNum, string msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetNextSenderMsgSeqNum()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetNextTargetMsgSeqNum()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetNextSenderMsgSeqNum(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetNextTargetMsgSeqNum(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncrNextSenderMsgSeqNum()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncrNextTargetMsgSeqNum()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetCreationTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Refresh()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
