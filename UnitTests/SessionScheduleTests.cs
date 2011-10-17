36b88cc4fb7fc87dc7488072e40c5e59e1889e76
chris busbey <cbusbey@connamara.com>
2011-10-17 13:15:21 -0500

    week session scheduling
    Former-commit-id: 912bd525ef83f478a3d8bfc8417b5495680edb83 [formerly e57dea3023e970a5cbd8c6ad4869b34b99c94be3] [formerly 4d2b3a5cbe003b430972f06910ea1872a93e974d [formerly 878d455f90e020a827d486f4ccde9c060bd31203]]
    Former-commit-id: ecbafa4c5ac3ef3ec35aac78077d7de1f65081bb [formerly 7b15953b08a0af9e5cee6d16d02c07cea4ee3d10]
    Former-commit-id: afa468b796942a71e2eb3d28dc7c9351b6157145

--- UnitTests/SessionScheduleTests.cs
+++ UnitTests/SessionScheduleTests.cs
@@ -55,6 +55,120 @@ namespace UnitTests
         }
 
         [Test]
+        public void testWeeklySessionOneDay()
+        {
+            QuickFix.Dictionary settings = new QuickFix.Dictionary();
+            settings.SetString(QuickFix.SessionSettings.START_TIME, "00:00:00");
+            settings.SetString(QuickFix.SessionSettings.END_TIME, "00:00:00");
+
+            //only on monday
+            settings.SetDay(QuickFix.SessionSettings.START_DAY, 1);
+            settings.SetDay(QuickFix.SessionSettings.END_DAY, 1);
+
+            QuickFix.SessionSchedule sched = new QuickFix.SessionSchedule(settings);
+
+            //a monday
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 17, 9, 43, 0)));
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 17, 23, 59, 59)));
+
+            //a tuesday
+            Assert.IsFalse(sched.IsSessionTime(new DateTime(2011, 10, 18, 9, 43, 0)));
+            Assert.IsFalse(sched.IsSessionTime(new DateTime(2011, 10, 18, 0, 0, 0)));
+        }
+
+        [Test]
+        public void testWeeklySessionMultiDay()
+        {
+            QuickFix.Dictionary settings = new QuickFix.Dictionary();
+            settings.SetString(QuickFix.SessionSettings.START_TIME, "00:00:00");
+            settings.SetString(QuickFix.SessionSettings.END_TIME, "00:00:00");
+
+            //only on monday-fri
+            settings.SetDay(QuickFix.SessionSettings.START_DAY, 1);
+            settings.SetDay(QuickFix.SessionSettings.END_DAY, 5);
+
+            QuickFix.SessionSchedule sched = new QuickFix.SessionSchedule(settings);
+
+            //a weekdays
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 17, 9, 43, 0)));
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 21, 23, 59, 59)));
+
+            //a weekend
+            Assert.IsFalse(sched.IsSessionTime(new DateTime(2011, 10, 16, 9, 43, 0)));
+            Assert.IsFalse(sched.IsSessionTime(new DateTime(2011, 10, 22, 0, 0, 0)));
+        }
+
+        [Test]
+        public void testWeeklySessionMultiDayOverlap()
+        {
+            QuickFix.Dictionary settings = new QuickFix.Dictionary();
+            settings.SetString(QuickFix.SessionSettings.START_TIME, "00:00:00");
+            settings.SetString(QuickFix.SessionSettings.END_TIME, "00:00:00");
+
+            //only on wed-mon
+            settings.SetDay(QuickFix.SessionSettings.START_DAY, 3);
+            settings.SetDay(QuickFix.SessionSettings.END_DAY, 1);
+
+            QuickFix.SessionSchedule sched = new QuickFix.SessionSchedule(settings);
+
+            //wed-monday
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 16, 9, 43, 0)));
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 17, 9, 43, 0)));
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 19, 9, 43, 0)));
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 20, 9, 43, 0)));
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 21, 9, 43, 0)));
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 22, 9, 43, 0)));
+
+            //tuesday
+            Assert.IsFalse(sched.IsSessionTime(new DateTime(2011, 10, 18, 9, 43, 0)));
+        }
+
+        [Test]
+        public void testWeeklySessionMultiDayHours()
+        {
+            QuickFix.Dictionary settings = new QuickFix.Dictionary();
+            settings.SetString(QuickFix.SessionSettings.START_TIME, "07:00:00");
+            settings.SetString(QuickFix.SessionSettings.END_TIME, "15:00:00");
+
+            //only on monday-fri
+            settings.SetDay(QuickFix.SessionSettings.START_DAY, 1);
+            settings.SetDay(QuickFix.SessionSettings.END_DAY, 5);
+
+            QuickFix.SessionSchedule sched = new QuickFix.SessionSchedule(settings);
+
+            //weekdays
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 17, 7, 30, 0)));
+            Assert.IsFalse(sched.IsSessionTime(new DateTime(2011, 10, 17, 15, 30, 0)));
+
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 21, 9, 59, 59)));
+            Assert.IsFalse(sched.IsSessionTime(new DateTime(2011, 10, 21, 15, 59, 59)));
+        }
+
+        [Test]
+        public void testWeeklySessionMultiDayHoursOverlap()
+        {
+            QuickFix.Dictionary settings = new QuickFix.Dictionary();
+            settings.SetString(QuickFix.SessionSettings.START_TIME, "15:00:00");
+            settings.SetString(QuickFix.SessionSettings.END_TIME, "07:00:00");
+
+            //only on monday-fri
+            settings.SetDay(QuickFix.SessionSettings.START_DAY, 1);
+            settings.SetDay(QuickFix.SessionSettings.END_DAY, 5);
+
+            QuickFix.SessionSchedule sched = new QuickFix.SessionSchedule(settings);
+
+            //weekdays
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 17, 15, 30, 0)));
+            Assert.IsFalse(sched.IsSessionTime(new DateTime(2011, 10, 17, 6, 30, 0)),"foo");
+
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 21, 15, 30, 59)));
+            Assert.IsTrue(sched.IsSessionTime(new DateTime(2011, 10, 22, 6, 59, 59)));
+            Assert.IsFalse(sched.IsSessionTime(new DateTime(2011, 10, 22, 7, 00, 1)));
+            Assert.IsFalse(sched.IsSessionTime(new DateTime(2011, 10, 22, 15, 30, 0)));
+        }
+        
+
+        [Test]
         public void testDailyIsSessionTime()
         {
             QuickFix.Dictionary settings = new QuickFix.Dictionary();
