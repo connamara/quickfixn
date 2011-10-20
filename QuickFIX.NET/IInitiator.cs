7d74d6fa2267b7bb03f378af52a707c50156cd36
Grant Birchmeier <gbirchmeier@connamara.com>
2011-10-20 15:01:06 -0500

    corrections to IInitiator refactor; add ResetOnXXX to executor.cfg

--- QuickFIX.NET/IInitiator.cs
+++ QuickFIX.NET/IInitiator.cs
@@ -18,29 +18,5 @@ namespace QuickFix
         void Stop(bool force);
 
         bool IsLoggedOn();
-
-        /// <summary>
-        /// Start connecting to targets.
-        /// </summary>
-        void OnStart();
-
-        /// <summary>
-        /// Connect and poll for events.
-        /// </summary>
-        /// <param name="timeout"></param>
-        /// <returns></returns>
-        bool OnPoll(double timeout);
-
-        /// <summary>
-        /// Stop a running initiator.
-        /// </summary>
-        void OnStop();
-
-        /// <summary>
-        /// Connect a session to its target.
-        /// </summary>
-        /// <param name="sessionID"></param>
-        /// <param name="settings"></param>
-        void DoConnect(SessionID sessionID, QuickFix.Dictionary settings);
     }
 }
