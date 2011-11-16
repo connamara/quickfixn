8b4028fc0ad1988a3292b08bc0f382aefa68ebdb
Richard Young <ryoung@connamara.com>
2011-11-16 09:34:40 -0600

    Downloads page now lists previous versions and includes a link to github tags page
    Former-commit-id: f7537edc55af9e49436d83693292de56de665e29 [formerly b06463728b6c43bf7e3d1381443d6a138a83a005] [formerly d7e7be5e40e1bdf8115e6ecbf1478f7bfc63c253 [formerly fe768b5d26b6a7a2784982d42f55f454d97ac22a]]
    Former-commit-id: 325d55edacb571e5d837e734358e53a96ff15d8c [formerly 635b23544539fe28a8409728441ddd40b0bef811]
    Former-commit-id: 6641e2905b69278f3efcf1f619fce1408506b214

--- scripts/update_download_page.rb
+++ scripts/update_download_page.rb
@@ -1,11 +1,16 @@
+require 'rubygems'
+require 'mustache'
+
 if ARGV[0].nil? or ARGV[1].nil?
   puts "Usage: ruby update_downloads_page.rb <PATH_TO_DOWNLOADS_PAGE> <VERSION_NO>"
   exit 1
 end
 
-page = File.join(Dir.pwd, ARGV[0])
-ver = ARGV[1]
+Mustache.template_file = File.join(File.dirname(__FILE__), 'downloads.mustache')
+view = Mustache.new
+view[:latest_version] = ARGV[1]
+view[:previous_versions] = prev_versions = `git tag -l`.split("\n")
 
-File.open(page, 'w') do |f|
-    f.puts "Download\n========\n\nThe latest version of QuickFIX/n is #{ver}\n\nYou can download it [here] (http://quickfixn.s3.amazonaws.com/quickfixn-#{ver}.zip)"
+File.open(File.join(Dir.pwd, ARGV[0]), 'w') do |f|
+    f.puts view.render
 end
