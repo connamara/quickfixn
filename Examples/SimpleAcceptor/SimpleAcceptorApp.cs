3126f0eb317b0f956eac2f7c34e1a59d519e4e7b
Grant Birchmeier <gbirchmeier@connamara.com>
2011-11-17 12:52:54 -0600

    new SimpleAcceptor project
    Former-commit-id: d495fed56c8921a57c5f798314627d798979bb72 [formerly 75ae3e58196d21bf1df509ca2033ee74c8ce5098] [formerly 867d87f1195ab1d1bae1d790d3e39d30038fd04b [formerly cfd902aa11ae395eb516c64b1420fff9b03d9b0a]]
    Former-commit-id: 211007efd7fe5d1ac34f34a6f6556391f69b8b63 [formerly 2a64d7105c234572ce570e8c1219dcfa65a0a04f]
    Former-commit-id: b371a6095e39a43e06ffe7259049da2e02ca290a

---
+++ Examples/SimpleAcceptor/SimpleAcceptorApp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using QuickFix;
+using QuickFix.Fields;
+
+namespace SimpleAcceptor
+{
+    /// <summary>
+    /// Just a simple server that will let you connect to it and ignore any
+    /// application-level messages you send to it.
+    /// Note that this app is *NOT* a message cracker.
+    /// </summary>
+
+    public class SimpleAcceptorApp : /*QuickFix.MessageCracker,*/ QuickFix.Application
+    {
+        #region QuickFix.Application Methods
+
+        public void FromApp(Message message, SessionID sessionID)
+        {
+            Console.WriteLine("IN:  " + message);
+            //Crack(message, sessionID);
+        }
+
+        public void ToApp(Message message, SessionID sessionID)
+        {
+            Console.WriteLine("OUT: " + message);
+        }
+
+        public void FromAdmin(Message message, SessionID sessionID) 
+        {
+            Console.WriteLine("IN:  " + message);
+        }
+
+        public void ToAdmin(Message message, SessionID sessionID)
+        {
+            Console.WriteLine("OUT:  " + message);
+        }
+
+        public void OnCreate(SessionID sessionID) { }
+        public void OnLogout(SessionID sessionID) { }
+        public void OnLogon(SessionID sessionID) { }
+        #endregion
+    }
+}
