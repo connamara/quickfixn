49360dfe4e6b2d8a43d1fbc5baf97eab3a08a02c
Grant Birchmeier <gbirchmeier@connamara.com>
2011-12-13 14:13:48 -0600

    change previous versions list ordering to be newest-first

--- scripts/update_download_page.rb
+++ scripts/update_download_page.rb
@@ -9,7 +9,7 @@ end
 Mustache.template_file = File.join(File.dirname(__FILE__), 'downloads.mustache')
 view = Mustache.new
 view[:latest_version] = ARGV[1]
-view[:previous_versions] = prev_versions = `git tag -l`.split("\n")
+view[:previous_versions] = prev_versions = `git tag -l`.split("\n").reverse
 
 File.open(File.join(Dir.pwd, ARGV[0]), 'w') do |f|
     f.puts view.render
