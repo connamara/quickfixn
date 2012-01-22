d2d28c7e5826129eb27060d854988b0cac95af2f
nisbus <nisbus@gmail.com>
2012-01-22 23:35:45 +0000

    Added nuspec and a script for building and publishing the nuget package

---
+++ QuickFIX.NET/CreateAndPushNugetPackage.bat
@@ -0,0 +1,4 @@
+﻿@echo off
+nuget pack QuickFix.csproj -Prop Configuration=Release
+for /f "delims=" %%x in ('dir *.nupkg /od /b') do set FileName=%%x
+nuget push %FileName%
--- QuickFIX.NET/Properties/AssemblyInfo.cs
+++ QuickFIX.NET/Properties/AssemblyInfo.cs
@@ -1,21 +1,20 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QuickFix")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("QuickFIX/N")]
+[assembly: AssemblyDescription("QuickFIX/n is a .NET port of QuickFIX, an open source C++ FIX engine")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("QuickFix.Properties")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyCompany("quickfixengine.org")]
+[assembly: AssemblyProduct("QuickFIX/N")]
+[assembly: AssemblyCopyright("Copyright © quickfixengine.org 2001-2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -25,7 +24,7 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
