ec2e802c94e0879c7defadef2c5bf5b1653c1212
Daniel Svensson <daniel.svensson@hotmail.se>
2013-01-08 11:38:50 +0100

    Add ssl support to initiators

--- QuickFIXn/SocketSettings.cs
+++ QuickFIXn/SocketSettings.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication;
 using System.Text;
 
 namespace QuickFix
@@ -11,5 +12,14 @@ namespace QuickFix
     public class SocketSettings
     {
         public bool SocketNodelay = true;
+
+        #region SSL Settings
+        public string HostName;
+        public bool ValidateServerCertificate = true;
+        public string CertificatePath;
+        public string CertificatePassword;
+        public SslProtocols SslProtocol = SslProtocols.Default;
+        public bool CheckCertificateRevocation = true;
+        #endregion
     }
 }
