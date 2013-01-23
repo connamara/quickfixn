330b57026bb3bce9c676b6528152fc0b3a96e4e8
Mike Gatny <mgatny@connamara.com>
2013-01-23 12:27:16 -0600

    write to outfile instead of stdout

--- AcceptanceTest/at_xml_to_nunit_xml.rb
+++ AcceptanceTest/at_xml_to_nunit_xml.rb
@@ -108,15 +108,17 @@ end
 end # module QuickFIXn
 
 if $0 == __FILE__
-    unless ARGV.size > 0
-        puts "usage: #{$0} ERB_FILE [XML_FILE...]"
+    unless ARGV.size >= 2
+        puts "usage: #{$0} OUT_FILE ERB_FILE [XML_FILE...]"
         raise SystemExit.new(2)
     end
     require 'erb'
+    out_file = ARGV.shift
     template = ARGV.shift
     xml_files = (ARGV.empty?) ? Dir.glob("AcceptanceTests_*.xml") : Array.new(ARGV)
     suite = QuickFIXn::Suite::create_from_acceptance_test_xml_list(xml_files)
     erb = ERB.new(File.read(template))
-    puts erb.result(binding)
+    result = erb.result(binding)
+    File.open(out_file, 'w') { |f| f.puts result }
 end
 
