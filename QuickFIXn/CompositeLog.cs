7a39564a0958662801a535aaf5186bdf8bf4542a
Grant Birchmeier <gbirchmeier@connamara.com>
2013-05-15 11:14:09 -0500

    add IDisposable support to CompositeLog (#180)

--- QuickFIXn/CompositeLog.cs
+++ QuickFIXn/CompositeLog.cs
@@ -8,33 +8,52 @@ namespace QuickFix
     {
         private ILog[] logs_;
 
+        private bool _disposed = false;
+
         public CompositeLog(ILog[] logs)
         {
             logs_ = logs;
-        }   
+        }
 
         public void Clear()
         {
+            DisposedCheck();
             foreach (var log in logs_)
-              log.Clear();
+                log.Clear();
         }
 
         public void OnIncoming(string msg)
         {
-          foreach (var log in logs_)
-            log.OnIncoming(msg);
+            DisposedCheck();
+            foreach (var log in logs_)
+                log.OnIncoming(msg);
         }
 
         public void OnOutgoing(string msg)
         {
-          foreach (var log in logs_)
-            log.OnOutgoing(msg);
+            DisposedCheck();
+            foreach (var log in logs_)
+                log.OnOutgoing(msg);
         }
 
         public void OnEvent(string s)
         {
-          foreach (var log in logs_)
-            log.OnEvent(s);
+            DisposedCheck();
+            foreach (var log in logs_)
+                log.OnEvent(s);
+        }
+
+        public void Dispose()
+        {
+            _disposed = true;
+            foreach (var log in logs_)
+                log.Dispose();
+        }
+
+        private void DisposedCheck()
+        {
+            if (_disposed)
+                throw new System.ObjectDisposedException(this.GetType().Name);
         }
     }
 }
