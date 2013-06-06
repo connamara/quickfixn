b5e63d8e58200ce455c337cab34bf4a7b5469f86
Dinesh <Buddylove_123@yahoo.co.uk>
2013-06-06 09:11:37 +0100

    Merge issue

--- QuickFIXn/IInitiator.cs
+++ QuickFIXn/IInitiator.cs
@@ -20,24 +20,6 @@ namespace QuickFix
         void Start();
 
         /// <summary>
-        /// Starts the specified factory.
-        /// </summary>
-        /// <param name="factory">The factory.</param>
-        /// <remarks>
-        /// Decura, new method
-        /// </remarks>
-        void Start(SessionFactory factory);
-
-        /// <summary>
-        /// Starts the specified factory.
-        /// </summary>
-        /// <param name="factory">The factory.</param>
-        /// <remarks>
-        /// Decura, new method
-        /// </remarks>
-        void Start(SessionFactory factory);
-
-        /// <summary>
         /// Logout existing session and close connection.  All open resources are released.
         /// </summary>
         void Stop();
