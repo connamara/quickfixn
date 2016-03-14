af32489b62a6a2f2b8cd06d434df93547a9f7059
Aidan Chisholm <aidan.chisholm@iress.co.uk>
2016-03-14 14:11:03 +0000

    .net 4.5 and x86 platform
    Changing fork to use .net 4.5 and x86 platform specific

--- build.bat
+++ build.bat
@@ -3,11 +3,13 @@ echo off
 set TARGET=%1
 set CONFIGURATION=%2
 set NET_VERSION=%3
+set PLATFORM=%4
 
 if "%1" == "" set TARGET=Rebuild
 if "%2" == "" set CONFIGURATION=Release
-if "%3" == "" set NET_VERSION=v3.5
+if "%3" == "" set NET_VERSION=v4.5
+if "%4" == "" set PLATFORM=x86
 
-set BUILD_CMD=MSBuild.exe QuickFIXn.sln /t:%TARGET% /p:Configuration=%CONFIGURATION%;TargetFrameworkVersion=%NET_VERSION%
+set BUILD_CMD=MSBuild.exe QuickFIXn.sln /t:%TARGET% /p:Configuration=%CONFIGURATION%;TargetFrameworkVersion=%NET_VERSION%;Platform=%PLATFORM%
 echo Build command: %BUILD_CMD%
 %BUILD_CMD%
