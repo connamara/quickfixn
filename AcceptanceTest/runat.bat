4d40030ff2d9ae8eb76635778eda6f93597f4a77
Zachary Herr <zherr@connamara.com>
2016-10-04 20:54:30 -0500

    reconfigure AcceptanceTest/runat.bat to run test server as executable
    Former-commit-id: 943a75907b42fe6a4edec5ac2d23104b3cfc2af9 [formerly a9d8a9675db1250dc481c0926864cac058f464bf] [formerly 1f4d6787215c69c6b5b2668c15971417f446db03 [formerly d76c485df39b8fc07bf0277a5a66873a2560f7f3]]
    Former-commit-id: 1cbb1e04a8606d83ffc411d3a14010d696781035 [formerly 3b4ad816a7334c3131df0883f907ec6eec91a677]
    Former-commit-id: 3d7f6d52c449e90630aaa666f12a5ba48de05100

--- AcceptanceTest/runat.bat
+++ AcceptanceTest/runat.bat
@@ -34,8 +34,10 @@ goto start
 
 call setup.bat %2
 
-REM release\at\atrun -t run -s "%DIR%\AcceptanceTest.exe cfg\at.cfg" -d . -c "ruby Runner.rb 127.0.0.1 %2 definitions\server\fix40\*.def definitions\server\fix41\*.def definitions\server\fix42\*.def definitions\server\fix43\*.def definitions\server\fix44\*.def definitions\server\fix50\*.def definitions\server\fix50sp1\*.def definitions\server\fix50sp2\*.def" -i .\  -o TestResult.xml 
-release\at\atrun -t run -s "%DIR%\AcceptanceTest.exe %CONF_FILE%" -d . -c "ruby Runner.rb 127.0.0.1 %2 %TESTS%" -i .\  -o TestResult.xml 
+start %DIR%\AcceptanceTest.exe %CONF_FILE%
+ruby Runner.rb 127.0.0.1 %2 %TESTS% > TestResult.xml
+taskkill /F /IM AcceptanceTest.exe
+
 if ERRORLEVEL 1 set RESULT=1
 type TestResult.xml
 goto quit
