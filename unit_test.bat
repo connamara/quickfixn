8b8f20aaf96f9afbee21505c2a19cebfa25f8b9d
Ami Bar <amib@eaglets.co.il>
2017-02-16 18:04:36 +0200

    Updated NUnit version

--- unit_test.bat
+++ unit_test.bat
@@ -1,11 +1,13 @@
 echo off
 
 set RESULT=0
+ 
+xcopy /s /y resources\nunit\* UnitTests\bin\Release\
 
 pushd UnitTests\bin\Release\
     if exist TestResult.xml del TestResult.xml
     if exist UnitTests.html del UnitTests.html
-    nunit-console UnitTests.dll
+    nunit3-console UnitTests.dll
     if ERRORLEVEL 1 set RESULT=1
 popd
 
