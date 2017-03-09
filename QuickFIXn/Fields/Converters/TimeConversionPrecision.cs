d5b862c780246171cd57edf7a2df6a85416ff3c9
Huw Jenkins <huw.jenkins@rapidsoftwareconsultancy.com>
2017-03-09 11:11:47 +0000

    Updating DateTimeConverter to be able to convert between microseconds
    and adding extra constructor to the DateTime Fields to allow the precision to be passed
    
    
    Former-commit-id: d082e417a53e63910387b4a64e0d244e4fda0cb2 [formerly e06734dca34d0075c61953b7868de24d630fa5f1] [formerly 37c3c5770f894266e18b512a0180c5b83b4f429f [formerly de4700d198b1db31bbe0fc81fac82c5a4e481fd1]]
    Former-commit-id: 40e2c7216862ff12c422dbbbf38925ebfdba2db5 [formerly 804663d1aeda517b3888ab582e529a5db3d7db29]
    Former-commit-id: 4ef371a4485bc73f383400df6ffa138c9328f09a

---
+++ QuickFIXn/Fields/Converters/TimeConversionPrecision.cs
@@ -0,0 +1,13 @@
+﻿
+namespace QuickFix.Fields.Converters
+{
+    /// <summary>
+    /// Enumerator to specify what precision times should be converted to
+    /// </summary>
+    public enum TimeConversionPrecision
+    {
+        Second,
+        Millisecond,
+        Microsecond
+    }
+}
