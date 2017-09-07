6ea4b6b71169739d415969a497b57026d02256ad
Albert Kapitanov <kapitanov@itglobal.ru>
2017-09-07 18:57:19 +0300

    Updated build scripts

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 
 if "%VERSION%" == "" set VERSION="1.7.0"
+if "%PACKAGE_ID%" == "" set PACKAGE_ID="QuickFix.Net"
 
 echo Restoring dependencies
 dotnet restore -v q /nologo /p:Version=%VERSION%
@@ -11,7 +12,7 @@ dotnet build -c Release -v q /nologo /p:Version=%VERSION%
 if %ERRORLEVEL% NEQ 0 goto exit
 
 echo Creating nuget package
-dotnet pack  -c Release -v q --no-build -o ../artifacts ./QuickFIXn/QuickFIX.csproj /nologo  /p:Version=%VERSION%
+dotnet pack  -c Release -v q --no-build -o ../artifacts ./QuickFIXn/QuickFIX.csproj /nologo  /p:Version=%VERSION% /p:PackageId=%PACKAGE_ID%
 echo Done
 
 :exit
--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@
 set -e
 
 if [ -z "$VERSION" ]; then
-    VERSION=1.7.0
+    VERSION="1.7.0"
+fi
+
+if [ -z "$PACKAGE_ID" ]; then
+    PACKAGE_ID="QuickFix.Net"
 fi
 
 echo "Restoring dependencies"
@@ -13,5 +17,5 @@ echo "Compiling project"
 dotnet build -c Release -v q /nologo /p:Version=$VERSION
 
 echo "Creating nuget package"
-dotnet pack  -c Release -v q --no-build -o ../artifacts ./QuickFIXn/QuickFIX.csproj /nologo /p:Version=$VERSION
+dotnet pack  -c Release -v q --no-build -o ../artifacts ./QuickFIXn/QuickFIX.csproj /nologo /p:Version=$VERSION /p:PackageId=$PACKAGE_ID
 echo Done
