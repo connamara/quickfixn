9b45c279b96efa119c90860cb7efa2f0fb2ef143
AIDAN CHISHOLM <aidan.chisholm@iress.co.uk>
2017-10-23 18:33:45 +0100

    New Project - Examples.FixHub
    Console project for testing purposes.

---
+++ Examples.FixHub/FixHub.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using QuickFix;
+
+namespace Examples.FixHub
+{
+    public class FixHub : IApplication
+    {
+        public void ToAdmin(Message message, SessionID sessionID)
+        {
+            
+        }
+
+        public void FromAdmin(Message message, SessionID sessionID)
+        {
+            
+        }
+
+        public void ToApp(Message message, SessionID sessionId)
+        {
+            
+        }
+
+        public void FromApp(Message message, SessionID sessionID)
+        {
+            Console.WriteLine("Message: " + message.ToString());
+
+            string deliverTo = message.Header.GetString(128);
+            SessionID dSession = new SessionID("FIX.4.2", "LSEHUB", deliverTo);
+            Session.SendToTarget(message, dSession);
+        }
+
+        public void OnCreate(SessionID sessionID)
+        {
+            Console.WriteLine("Created:" + sessionID.ToString());
+        }
+
+        public void OnLogout(SessionID sessionID)
+        {
+            Console.WriteLine("OnLogout:" + sessionID.ToString());
+        }
+
+        public void OnLogon(SessionID sessionID)
+        {
+            Console.WriteLine("OnLogon:" + sessionID.ToString());
+        }
+    }
+}
---
+++ Examples.FixHub/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using QuickFix;
+
+namespace Examples.FixHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SessionSettings settings = new SessionSettings("ini.cfg");
+            IApplication fixHub = new FixHub();
+            IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+            ILogFactory logFactory = new FileLogFactory(settings);
+            ThreadedSocketAcceptor acceptor = new ThreadedSocketAcceptor(fixHub, storeFactory, settings, logFactory);
+            
+            acceptor.Start();
+            Console.ReadKey();
+            acceptor.Stop();
+
+            
+        }
+    }
+}
