e15757aaac23e7b5d4df9144b5c42e952b05ea83
Philipe de Liz Pereira <philipelizp+github@gmail.com>
2018-02-23 16:37:07 -0300

    Correct ordering in message trailer

--- QuickFIXn/Message/Message.cs
+++ QuickFIXn/Message/Message.cs
@@ -30,6 +30,8 @@ namespace QuickFix
 
     public class Trailer : FieldMap
     {
+        public int[] TRAILER_FIELD_ORDER = { Tags.SignatureLength, Tags.Signature, Tags.CheckSum };
+
         public Trailer()
             : base()
         { }
@@ -37,6 +39,16 @@ namespace QuickFix
         public Trailer(Trailer src)
             : base(src)
         { }
+
+        public override string CalculateString()
+        {
+            return base.CalculateString(new StringBuilder(), TRAILER_FIELD_ORDER);
+        }
+
+        public override string CalculateString(StringBuilder sb, int[] preFields)
+        {
+            return base.CalculateString(sb, TRAILER_FIELD_ORDER);
+        }
     }
 
     /// <summary>
