c0155525f6d3ea68898ab1cca16ce01310a0a970
Mike Gatny <mgatny@gmail.com>
2020-03-05 09:38:28 -0500

    Add using directives back in to fix build errors

--- QuickFIXn/Transport/StreamFactory.cs
+++ QuickFIXn/Transport/StreamFactory.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.IO;
 using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
