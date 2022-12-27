f9ff6cb2ff1587974cc46877e99b9d448dc5001b
Grant Birchmeier <gbirchmeier@connamara.com>
2022-12-27 09:17:36 -0500

    get net6.0 building on *nix
    specifically mac

--- acceptance_test.ps1
+++ acceptance_test.ps1
@@ -53,7 +53,7 @@ function RunSuite
         [string]$Configuration,
 
         [Parameter(Mandatory, Position=1, ValueFromPipeline)]
-        [ValidateSet('net461','netcoreapp2.1')]
+        [ValidateSet('net6.0','net461','netcoreapp2.1')]
         [string[]]$Framework,
 
         [switch]$UseWsl
@@ -100,7 +100,7 @@ try {
 
     Remove-Item AcceptanceTests_*.xml
 
-    'net461', 'netcoreapp2.1' | RunSuite -Configuration $Configuration -UseWsl:$($UseWsl -and $UseWsl.IsPresent)
+    'net6.0' | RunSuite -Configuration $Configuration -UseWsl:$($UseWsl -and $UseWsl.IsPresent)
 } finally {
     Pop-Location -StackName AcceptanceTest
 
