28da79ca4cb4643087cfd4111c8170e2d614683a
Grant Birchmeier <gbirchmeier@connamara.com>
2024-05-21 17:36:36 -0500

    stop capitalizing TimestampPrecision wrong
    it's not "TimeStamp", argh
    
    I'm only fixing it in messages and docs.
    I'm not going to change var/constant names
    right now even though I kind of want to.

--- QuickFIXn/SessionSettings.cs
+++ QuickFIXn/SessionSettings.cs
@@ -55,7 +55,7 @@ namespace QuickFix
         public const string LOGOUT_TIMEOUT = "LogoutTimeout";
         public const string SEND_REDUNDANT_RESENDREQUESTS = "SendRedundantResendRequests";
         public const string RESEND_SESSION_LEVEL_REJECTS = "ResendSessionLevelRejects";
-        public const string TIMESTAMP_PRECISION = "TimeStampPrecision";
+        public const string TIMESTAMP_PRECISION = "TimestampPrecision";
         public const string ENABLE_LAST_MSG_SEQ_NUM_PROCESSED = "EnableLastMsgSeqNumProcessed";
         public const string MAX_MESSAGES_IN_RESEND_REQUEST = "MaxMessagesInResendRequest";
         public const string SEND_LOGOUT_BEFORE_TIMEOUT_DISCONNECT = "SendLogoutBeforeDisconnectFromTimeout";
