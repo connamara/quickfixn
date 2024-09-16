0d4b1b1f0a5feaa09f0e7441721d774b25580c35
Grant Birchmeier <gbirchmeier@connamara.com>
2024-09-16 12:54:23 -0500

    unit tests for CheckSumConverter

--- QuickFIXn/Fields/Converters/CheckSumConverter.cs
+++ QuickFIXn/Fields/Converters/CheckSumConverter.cs
@@ -1,13 +1,22 @@
-﻿
-namespace QuickFix.Fields.Converters
+﻿namespace QuickFix.Fields.Converters
 {
     public static class CheckSumConverter
     {
+        /// <summary>
+        /// Convert input string to int
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
         public static int Convert(string i)
         {
             return IntConverter.Convert(i);
         }
 
+        /// <summary>
+        /// Convert input int to 3-character string
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
         public static string Convert(int i)
         {
             return i.ToString("000");
